7400a5b0704124d19d608f7b6e25cb49e526e141
priyan-coder <rajamohanpriyan@gmail.com>
2021-01-12 15:02:05 +0800

    arrow functions

--- play.js
+++ play.js
@@ -7,6 +7,20 @@
 // If you intend on changing the value assigned to a variable ==> use let
 // But if you intend to retain that variable as a constant ==> use const
 
+// Strins, numbers, booleans, undefines and null are primitive types ==> stored in a stack
+// Objects and arrays are reference types ==> the pointer is stored in a stack while the object or the array itself is stored in the heap
+// Checkout this article and video for full understanding on mutability, primitive vs reference types ==> https://academind.com/tutorials/reference-vs-primitive-values/
+// Basically for objects, nested objects or arrays and nested arrays, etc ==> You need to copy out every layer and then make changes to the copy
+// Else, changes to the copied obejct/ array or the original object/ array for its nested reference types will reflect on both and cause issues
+// This is solved using immer, a library or using redux ==> createReducer
+
+// arrow functions help to ensure that the this keyword refers to the object which calls the function
+// lets say addName is a function within a class
+// method 1 ==> this.addName.bind(this)
+// method 2 ==> () => {this.addName()}
+// this keyword is used to access any variable or method stated within the class
+// Checkout ==> https://academind.com/tutorials/this-keyword-function-references/
+
 // string
 const name = "Max";
 console.log(name);
@@ -18,8 +32,9 @@ const hasHobbies = true;
 age = 30;
 
 // Function parameteres are local variables
-function summarizedUser(userName, ageOfUser, hobbies) {
+// changed to arrow fn
+const summarizedUser = (userName, ageOfUser, hobbies) => {
   return userName + " " + ageOfUser + " " + hobbies;
-}
+};
 
 console.log(summarizedUser(name, age, hasHobbies));
