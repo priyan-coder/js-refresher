e33563389658fb50658d38316d3de40284773ea7
priyan-coder <rajamohanpriyan@gmail.com>
2021-01-12 20:00:39 +0800

    more on arrays

--- arrays.js
+++ arrays.js
@@ -4,9 +4,13 @@
 // The find() method returns the value of the first element in the provided array that satisfies the provided testing function. If no values satisfies the testing function, undefined is returned.
 // The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
 
+// By adding a new element or changing stuff within this array we dont violate the const
+// Because arrays / objects are reference types and the changes that we do ==> dont change the address held by the pointer hobbies
 const hobbies = ["Sports", "Cooking"];
 
 // looping through
 for (let hobby of hobbies) {
   console.log(hobby);
 }
+hobbies.push("Programming");
+console.log(hobbies);
