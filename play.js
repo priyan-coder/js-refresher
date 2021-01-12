cd6c8da865977a2233411872c0cb2077fa13378e
priyan-coder <rajamohanpriyan@gmail.com>
2021-01-12 11:57:01 +0800

    let vs const vs var

--- play.js
+++ play.js
@@ -2,13 +2,20 @@
 // https://developer.mozilla.org/en-US/docs/Learn/JavaScript
 
 // Global variables
+
+// var is a keyword that creates new variables but it is outdated
+// If you intend on changing the value assigned to a variable ==> use let
+// But if you intend to retain that variable as a constant ==> use const
+
 // string
-var name = "Max";
+const name = "Max";
 console.log(name);
 // number
-var age = 29;
+let age = 29;
 // boolean
-var hasHobbies = true;
+const hasHobbies = true;
+
+age = 30;
 
 // Function parameteres are local variables
 function summarizedUser(userName, ageOfUser, hobbies) {
